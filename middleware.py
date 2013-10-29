4a3eff118d862c1ad6c3b733f672fab0d18d93ba
Egor Balyshev <frag.trials@gmail.com>
2013-10-30 00:18:08 +0800

    Working proof of concept prototype

---
+++ middleware.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from funcy import *
+import requests
+
+EXTERNAL_HOST = 'http://www.redom.ru'
+DEFAULT_ENCODING = 'utf-8'
+
+"""
+TODO:
+ - accurate settings
+ - threads to reduce processing time
+ - permissive include directive support (virtual vs file, spaces everywhere)
+"""
+
+class SsiMiddleware(object):
+    def process_response(self, request, response):
+        includes = set(re_all(r'<!--# include virtual=".*?" -->', response.content))
+        responses = {}
+        for include in includes:
+            uri = re_find(r'virtual="(.*?)"', include)
+            url = '%s%s' % (EXTERNAL_HOST, uri)
+            r = requests.get(url)
+            if 'charset' not in r.headers.get('content-type', ''):
+                r.encoding = DEFAULT_ENCODING
+            responses[include] = r.text.encode('utf-8')
+        for include, replacement in responses.items():
+            response.content = response.content.replace(include, replacement)
+
+        return response
